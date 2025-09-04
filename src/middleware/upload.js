746b17bccc5f387368afb735f3655058036d0b4d
Marcos Nalin <marcosnalin3@gmail.com>
2025-09-04 12:32:24 -0300

    Update upload.js
    config do nome do arquivo

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,7 +1,7 @@
 const multer = require('multer');
 const path = require('path');
 
-// Configuração de destino e nome do arquivo
+// Config de destino/ nome do arquivo 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'uploads/');
